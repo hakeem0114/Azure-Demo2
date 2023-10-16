64ac166b64a92dbf842f8c4c8c7c80f6dabcc11b
hakeem0114 <hakeem0114@gmail.com>
2023-10-16 11:19:32 -0400

    Modified title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 class="text-3xl">Vite + React</h1>
+      <h1 class="text-3xl">Azure Demo2</h1>
       <div  class="hover:bg-sky-300" className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
